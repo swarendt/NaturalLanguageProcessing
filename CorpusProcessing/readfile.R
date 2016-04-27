ec8b25018ed43e5c4c2c85a77743868cf883f7e1
swarendt <scott.w.arendt@gmail.com>
2016-04-26 20:25:25 -0500

    Updates to Corpus processing

--- CorpusProcessing/readfile.R
+++ CorpusProcessing/readfile.R
@@ -4,17 +4,15 @@ library(tm)
 setwd("C:/Development/R Projects/Capstone/Dataset")
 
 # con <- file("en_US.blogs.txt", open="r")
-#con <- file("twitter1000.txt", open="r")
-#twitter <- readLines(con)
-#close(con)
-#myCorpus <- Corpus(VectorSource(twitter))
-
-# multiple text files
-res <- textfile("*1000.txt", cache = FALSE)
-summary(corpus(res), 5)
+con <- file("twitter100000.txt", open="rb")
+twitter <- readLines(con)
+close(con)
+# myCorpus <- Corpus(VectorSource(twitter))
+myCorpus <- Corpus(VectorSource(twitter[1:100000]))
 
 myCorpus <- tm_map(myCorpus, content_transformer(tolower))
-myCorpus <- tm_map(myCorpus, PlainTextDocument)
+# myCorpus <- tm_map(myCorpus, PlainTextDocument)
+myCorpus <- tm_map(myCorpus, removeNumbers)
 myCorpus <- tm_map(myCorpus, removePunctuation)
 # myCorpus = tm_map(myCorpus, removeWords, stopwords("english"))
 # myCorpus = tm_map(myCorpus, stemDocument) 
@@ -23,11 +21,18 @@ myCorpus <- tm_map(myCorpus, removePunctuation)
 # hateCount <- sum(str_count(res, "hate"))
 # max(str_length(res))
 
+UnigramTokenizer <-
+  function(x)
+    unlist(lapply(ngrams(words(x), 1), paste, collapse = " "), use.names = FALSE)
+ 
 BigramTokenizer <-
-    function(x)
-        unlist(lapply(ngrams(words(x), 2), paste, collapse = " "), use.names = FALSE)
-
+  function(x)
+    unlist(lapply(ngrams(words(x), 2), paste, collapse = " "), use.names = FALSE)
+ 
+TrigramTokenizer <-
+  function(x)
+    unlist(lapply(ngrams(words(x), 3), paste, collapse = " "), use.names = FALSE)
+ 
 tdm <- TermDocumentMatrix(myCorpus, control = list(tokenize = BigramTokenizer))
-inspect(removeSparseTerms(tdm[, 1:10], 0.7))
-inspect(tdm)
 
+inspect(tdm[1:100,1:10])
