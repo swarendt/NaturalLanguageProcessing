be32e7fdb1d3e545cd896d05a0d61ea280df0286
Scott Arendt <scott.w.arendt@gmail.com>
2016-05-02 16:34:53 -0500

    Apostrophe handling fix and beginning use of slam

--- CorpusProcessing/readfile.R
+++ CorpusProcessing/readfile.R
@@ -3,44 +3,38 @@ library(tm)
 library(slam)
 # library(tau)
 
-setwd("C:/Development/R Projects/Capstone/Dataset")
-# setwd("C:/Development/R Projects/Dataset")
+# setwd("C:/Development/R Projects/Capstone/Dataset")
+setwd("C:/Development/R Projects/Dataset")
 
-con <- file("en_US.blogs.txt", encoding='UTF-8')
-# con <- file("en_US.twitter.txt", encoding='UTF-8')
+# con <- file("en_US.blogs.txt", encoding='UTF-8')
+con <- file("en_US.twitter.txt", encoding='UTF-8')
 # con <- file("en_US.news.txt", encoding='UTF-8')
 textSource <- readLines(con)
 close(con)
-myCorpus <- Corpus(VectorSource(textSource[1:100000]))
+myCorpus <- Corpus(VectorSource(textSource[1:1000]))
 rm(textSource)
 rm(con)
 
 for(j in seq(myCorpus))   
 {   
-  myCorpus[[j]] <- gsub("[^0-9A-Za-z///' ]", "", myCorpus[[j]])
-  # myCorpus[[j]] <- gsub("[^[:alnum:]///' ]", "", myCorpus[[j]])
+  myCorpus[[j]] <- gsub("[^[:alnum:]' ]", "", myCorpus[[j]])
 } 
 
 myCorpus <- tm_map(myCorpus, PlainTextDocument)
 myCorpus <- tm_map(myCorpus, content_transformer(tolower))
 
-# I am not yet sure about how to handle numbers.  Here's why:
-# If I include them, there are lots of additional tokens that may not be mostly noise.
-# If I remove them and intend to have my solution "learn" from the user, I may be discounting their writing style
 # myCorpus <- tm_map(myCorpus, removeNumbers)
 
-myCorpus <- tm_map(myCorpus, removePunctuation)
 myCorpus <- tm_map(myCorpus, stripWhitespace)
-myCorpus = tm_map(myCorpus, removeWords, stopwords("english"))
+
+# I have decided not to remove stopwords, it alters the results that I want
+# myCorpus = tm_map(myCorpus, removeWords, stopwords("english"))
+
 # Profanity Filter
-myCorpus = tm_map(myCorpus, removeWords, c("shit","fuck","fucker"))
+myCorpus = tm_map(myCorpus, removeWords, c("shit","fuck","fucker","ass","asshole"))
 
 # I have deliberately chosen not to use stemming
 # myCorpus = tm_map(myCorpus, stemDocument) 
-
-# loveCount <- sum(str_count(res, "love"))
-# hateCount <- sum(str_count(res, "hate"))
-# max(str_length(res))
 
 UnigramTokenizer <-
   function(x)
@@ -68,14 +62,23 @@ unitdmcount <- slam::row_sums(unitdm, na.rm = T)
 unitdmMatrix <- as.matrix(unitdmcount)
 write.csv(unitdmMatrix, 'blogs100kunigram.csv')
 
+bitdmcount <- slam::row_sums(bitdm, na.rm = T)
+bitdmMatrix <- as.matrix(bitdmcount)
+write.csv(bitdmMatrix, 'blogs100kbigram.csv')
+
+tritdmcount <- slam::row_sums(tritdm, na.rm = T)
+tritdmMatrix <- as.matrix(tritdmcount)
+write.csv(tritdmMatrix, 'blogs100ktrigram.csv')
+
+
 inspect(unitdm)
 inspect(bitdm)
 inspect(tritdm)
-head(unittdmcount)
+head(unitdmcount)
 
 rm(unitdm)
 rm(bitdm)
 rm(tritdm)
 rm(myCorpus)
-rm(unittdmcount)
+rm(unitdmcount)
 rm(unitdmMatrix)
