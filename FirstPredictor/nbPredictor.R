f2a6788888795154cba825bec6efb2626c5e2ad1
Scott Arendt <scott.w.arendt@gmail.com>
2016-05-10 14:10:18 -0500

    Updating the predictor script

--- FirstPredictor/nbPredictor.R
+++ FirstPredictor/nbPredictor.R
@@ -1,11 +1,12 @@
 library(dplyr)
+library(klaR)
 
 # Scott Arendt
 # May 8 2016
 # Using Naive Bayes to do predictions
 
-setwd("C:/Development/R Projects/Capstone/Dataset")
-# setwd("C:/Development/R Projects/Dataset")
+# setwd("C:/Development/R Projects/Capstone/Dataset")
+setwd("C:/Development/R Projects/Dataset")
 
 
 mydata = read.csv("BigramSplitTrain.csv")  # read csv file 
@@ -15,6 +16,5 @@ filter(mydata, NextWord == "water")
 mydata[1,]
 mydata[NextWord == "water",]
 
-subset.data.frame(mydata, Frequency > 1)
-louiedata <- subset.data.frame(mydata, Unigram == "louie" & PhraseCount > 1)
-louiedata
+submyData <- subset.data.frame(mydata, Frequency > 3)
+fit <- NaiveBayes(NextWord ~ ., data = submyData)
