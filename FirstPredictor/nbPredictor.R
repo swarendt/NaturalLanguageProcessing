ba27f319fca95f97f355a606d607e4ab5cde6bf5
swarendt <scott.w.arendt@gmail.com>
2016-05-10 20:01:11 -0500

    Testing is showing that Naive Bayes won't work Starting to lean toward a function based on the highest frequency nGram

--- FirstPredictor/nbPredictor.R
+++ FirstPredictor/nbPredictor.R
@@ -1,20 +1,38 @@
 library(dplyr)
+library(caret)
+library(e1071)
 library(klaR)
 
 # Scott Arendt
 # May 8 2016
 # Using Naive Bayes to do predictions
 
-# setwd("C:/Development/R Projects/Capstone/Dataset")
-setwd("C:/Development/R Projects/Dataset")
+setwd("C:/Development/R Projects/Capstone/Dataset")
+#setwd("C:/Development/R Projects/Dataset")
 
 
 mydata = read.csv("BigramSplitTrain.csv")  # read csv file 
 dim(mydata)
+submyData <- subset.data.frame(mydata, PhraseCount > 3)
 
-filter(mydata, NextWord == "water")
-mydata[1,]
-mydata[NextWord == "water",]
+# e1071 naiveBayes
+set.seed(3456)
+fit1 <- naiveBayes(submyData$NextWord, submyData$Unigram)
 
-submyData <- subset.data.frame(mydata, Frequency > 3)
-fit <- NaiveBayes(NextWord ~ ., data = submyData)
+# klaR NaiveBayes
+set.seed(3456)
+fit2 <- NaiveBayes(submyData$NextWord, submyData$Unigram, usekernal = FALSE, fL = 0)
+
+# A sample here - avoids machine learning
+# Here we are going to only use our database
+# Kind of makes sense here
+# Use compressed data rdata
+f <- function(queryHistoryTab, query, n = 2) {
+  require(tau)
+  trigrams <- sort(textcnt(rep(tolower(names(queryHistoryTab)), queryHistoryTab), method = "string", n = length(scan(text = query, what = "character", quiet = TRUE)) + 1))
+  query <- tolower(query)
+  idx <- which(substr(names(trigrams), 0, nchar(query)) == query)
+  res <- head(names(sort(trigrams[idx], decreasing = TRUE)), n)
+  res <- substr(res, nchar(query) + 2, nchar(res))
+  return(res)
+}
