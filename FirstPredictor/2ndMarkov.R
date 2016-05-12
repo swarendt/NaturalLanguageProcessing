08523afe55dbc9ef9b04070c6c24bbdf6b54ef53
swarendt <scott.w.arendt@gmail.com>
2016-05-11 20:34:15 -0500

    Function to utilize trigrams

--- FirstPredictor/2ndMarkov.R
+++ FirstPredictor/2ndMarkov.R
@@ -13,3 +13,54 @@
 
 # It turns out that Arendt's Brute Force Predictor goes by another name, Second
 # Order Markov Chain.  
+
+library(tau)
+
+setwd("C:/Development/R Projects/Capstone/Dataset")
+#setwd("C:/Development/R Projects/Dataset")
+
+mydata = read.csv("TrigramSplitTrainTop1.csv")  # read csv file 
+dim(mydata)
+submyData <- subset.data.frame(mydata, PhraseCount > 3)
+
+predictTrigram <- function(queryHistoryTab, inputText, n = 1) {
+
+  trigrams <- 
+    sort(textcnt(rep(names(queryHistoryTab), queryHistoryTab), method = "string"
+                    , n = length(scan(text = inputText, what = "character", quiet = TRUE)) 
+                 + 1))
+  return(trigrams)
+  
+  inputText <- tolower(inputText)
+    idx <- which(substr(names(trigrams), 0, nchar(inputText)) == inputText)
+    res <- head(names(sort(trigrams[idx], decreasing = TRUE)), n)
+    res <- substr(res, nchar(inputText) + 2, nchar(res))
+    return(res)
+}
+predictTrigram(c("Can of beer" = 3, "can of Soda" = 2, "A can of water" = 1
+    , "Buy me a can of soda, please" = 2), "Can of")
+
+
+queryHistoryTab <- c("Can of beer" = 3, "can of Soda" = 2, "A can of water" = 1
+                     , "Buy me a can of soda, please" = 2)
+inputText <- "Can of"
+n <- 1
+
+  
+  trigrams <- 
+    sort(textcnt(rep(names(queryHistoryTab), queryHistoryTab), method = "string"
+                 , n = length(scan(text = inputText, what = "character", quiet = TRUE)) 
+                 + 1))
+  trigrams
+  
+  inputText <- tolower(inputText)
+  inputText
+  
+  idx <- which(substr(names(trigrams), 0, nchar(inputText)) == inputText)
+  idx
+  res <- head(names(sort(trigrams[idx], decreasing = TRUE)), n)
+  res
+  res <- substr(res, nchar(inputText) + 2, nchar(res))
+  res
+
+  
