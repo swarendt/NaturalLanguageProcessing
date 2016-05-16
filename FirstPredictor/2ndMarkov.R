4cbd1bc7392e6f4dafca22b1db3e3ed56d374bf4
Scott Arendt <scott.w.arendt@gmail.com>
2016-05-16 15:12:47 -0500

    Expansion of prediction algorithm

--- FirstPredictor/2ndMarkov.R
+++ FirstPredictor/2ndMarkov.R
@@ -15,26 +15,50 @@
 # Order Markov Chain.  
 
 library(tau)
+library(stringr)
 
 #setwd("C:/Development/R Projects/Capstone/Dataset")
 setwd("C:/Development/R Projects/Dataset")
 
-mydata = read.csv("TrigramSplitTrainTop1.csv")  # read csv file 
-dim(mydata)
-submyData <- subset.data.frame(mydata, PhraseCount > 3)
+myData = read.csv("TrigramSplitTrain.csv")  # read csv file 
+# dim(mydata)
+submyTrigrams <- subset.data.frame(myData, FrequencyCount > 3)
+
+myData = read.csv("BigramSplitTrain.csv")  # read csv file 
+submyBigrams <- subset.data.frame(myData, FrequencyCount > 3)
+rm(myData)
 
 # This function will return the most popular word for a trigram, given an input of a bigram
 predictTrigram <- function(inputText) {
 
   inputText <- tolower(inputText)
-  
-  trigrams <- submyData[submyData$Bigram==inputText,]
-  
+
+  # Check Trigram data frame for suggestion
+  trigrams <- submyTrigrams[submyTrigrams$Bigram==inputText,]
   res <- as.character(trigrams[1,]$NextWord)
+  
+  # If no trigram suggestion, move on to bigram
+  if (is.na(res)) {
+    # remove first word
+    inputText <- word(inputText,-1)
+    
+    bigrams <- submyBigrams[submyBigrams$Unigram==inputText,]
+    res <- as.character(bigrams[1,]$NextWord)
+  }
+
+  
+ # If no bigram suggestion, move on to unigram
+  if (is.na(res)) {
+    
+    res <- 'OOV'
+  }
+  
   return(res)
 }
 
 inputText <- "small business"
-predictTrigram(inputText)
 
-  
+inputText <- "superf Lousy"
+
+inputText <- "Arendt credited"
+predictTrigram(inputText)
